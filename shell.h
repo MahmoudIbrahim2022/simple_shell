283841284ae3adf26b2600e5cbc4c6f673c586a1
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-08-16 14:22:32 -0700

    Add builtin_emulators2.c File

--- shell.h
+++ shell.h
@@ -232,7 +232,7 @@ int renumber_history(info_t *info);
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
 size_t print_list_str(const list_t *);
-int delete_node_at_index(list_t *, unsigned int);
+int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
 
 /* liststr2.c module */
